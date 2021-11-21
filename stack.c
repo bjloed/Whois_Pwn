e41c40128c3282b59be23d7312b4a3dc74636f06
Jongseong Kim <bjloed@ajou.ac.kr>
2021-11-22 04:55:11 +0900

    Update stack.c

--- stack.c
+++ stack.c
@@ -9,8 +9,9 @@ void hexreturn(QWORD num) {
 void stackview(char *name) {
 	puts("[STACK]");
 	printf("Name size: %d\n", 40);
-	printf("Address: %p\n", name);
-	printf("Press 'exit' to exit\n");
+	printf("Address: %p\n\n", name);
+	printf("Input 'hex' to change mode\n");
+	printf("Input 'exit' to exit\n\n");
 	printf("+------------------+ 0x0    <= name\n");
 	hexreturn(*(QWORD *)name);
 	write(1, temp, 22);
@@ -36,17 +37,46 @@ void stackview(char *name) {
 	hexreturn(*(QWORD *)(name + 56));
 	write(1, temp, 22);
 	printf("+------------------+\n");
+	printf("name => %s\n\n", name);
+}
+
+void hexinput(unsigned char *name) {
+	unsigned char hextemp[513] = "";
+	unsigned char newhextemp[256] = "";
+	unsigned char temptemp[8] = "";
+
+	int cnt = 0;
+	puts("Example) 01020304 -> '\\x01\\x02\\x03\\x04'");
+	read(0, hextemp, 512);
+
+	for (int i = 0; i < strlen(hextemp); i += 4) {
+		newhextemp[cnt++] = *(hextemp + i + 2);
+		newhextemp[cnt++] = *(hextemp + i + 3);
+	}
+	for (int i = 0; i < strlen(newhextemp); i += 2) {
+		int num;
+		temptemp[0] = newhextemp[i];
+		temptemp[1] = newhextemp[i + 1];
+
+		num = (int)strtol(temptemp, NULL, 16);
+		name[i / 2] = num & 0xff;
+	}
 }
 
 int main() {
-	char name[40] = "";
-
+	unsigned char name[40] = "";
+	unsigned char command[6] = "";
 	while (1) {
 		stackview(name);
-		puts("What is your name?");
-		gets(name);
-		if (!strcmp("exit", name))
+		puts("Input Command.");
+		read(0, command, 6);
+		if (!strncmp("exit", command, 4))
 			break;
+		else if (!strncmp("hex", command, 3))
+			hexinput(name);
+		else if (!strncmp("ASCII", command, 5)) {
+			puts("What is your name?");
+			read(0, name, 256);
+		}
 	}
-
 }
